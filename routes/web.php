1b245f5be61f4d592f2b123a9bbc108972c71897
dragon1211 <vikt0r1211@hotmail.com>
2021-09-15 20:10:14 -0700

    fixed auth

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('root');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
