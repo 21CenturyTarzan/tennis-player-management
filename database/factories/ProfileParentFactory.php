b08df2ccf7a41050e94d662ae3bd3b7b1264a181
dragon1211 <vikt0r1211@hotmail.com>
2021-09-20 11:39:55 -0700

    add seeder, factory

---
+++ database/factories/ProfileParentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ProfileParent;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProfileParentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ProfileParent::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
